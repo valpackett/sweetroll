8c679db8619c467ad02fc6abd0f9624cdcb44a11
Greg V <greg@unrelenting.technology>
2016-01-08 15:32:10 +0300

    json micropub

--- library/Sweetroll/Micropub/Request.hs
+++ library/Sweetroll/Micropub/Request.hs
@@ -11,9 +11,12 @@ module Sweetroll.Micropub.Request (
 import           ClassyPrelude hiding (first)
 import           Control.Arrow (first)
 import           Control.Error.Util (hush)
+import           Control.Lens
 import           Data.Aeson
+import           Data.Aeson.Lens
 import           Data.Attoparsec.Text as AP
 import           Servant
+import           Sweetroll.Util
 
 type ObjType = Text
 type ObjProperties = Object
@@ -22,7 +25,35 @@ type ObjSyndication = Text
 data MicropubRequest = Create ObjType ObjProperties [ObjSyndication]
 
 instance FromJSON MicropubRequest where
+  parseJSON v@(Object _) =
+    case v ^? key "mp-action" . _String of
+      Nothing → return $ Create (fromMaybe "h-entry" $ firstStr v $ key "type")
+                                (fromMaybe (object [] ^. _Object) $ v ^? key "properties" . _Object)
+                                (v ^.. key "mp-syndicate-to" . values . _String)
+      _ → fail "Unknown action type"
   parseJSON _ = mzero
+
+instance FromFormUrlEncoded MicropubRequest where
+  fromFormUrlEncoded f =
+    case lookup "mp-action" f of
+      Nothing → let v@(Object o') = formToObject f
+                    o = deleteMap "h" o'
+                    h = "h-" ++ fromMaybe "entry" (lookup "h" f)
+                    synd = (v ^.. key "mp-syndicate-to" . values . _String) ++
+                           (v ^.. key "syndicate-to" . values . _String) in
+                      Right $ Create h o synd
+      _ → Left "Unknown action type"
+
+formToObject ∷ [(Text, Text)] → Value
+formToObject f = foldl' assignProp (object []) $ (map . first) parseKey f
+  where parseKey x = fromMaybe [ x ] $ hush $ parseOnly formKey x
+        assignProp (Object o) ([k], v) = Object $ insertWith concatJSON k (toJSON [ v ]) o
+        assignProp (Object o) (k : k' : ks, v) = Object $ insertWith (\_ o' → assignProp o' (k' : ks, v)) k (assignProp (object []) (k' : ks, v)) o
+        assignProp x _ = x
+        concatJSON (Array v1) (Array v2) = Array $ v1 ++ v2
+        concatJSON (Array v1) _ = Array v1
+        concatJSON _ (Array v2) = Array v2
+        concatJSON _ _ = Null
 
 formKey ∷ Parser [Text]
 formKey = do
@@ -34,23 +65,3 @@ formKey = do
     return s
   void $ option '_' $ char '[' >> char ']'
   return $ firstKey : filter (not . null) restKeys
-
-formToObject ∷ [(Text, Text)] → Value
-formToObject f = foldl' assign (object []) $ (map . first) parseKey f
-  where parseKey x = fromMaybe [ x ] $ hush $ parseOnly formKey x
-        assign (Object o) ([k], v) = Object $ insertWith concatJSON k (toJSON [ v ]) o
-        assign (Object o) (k : k' : ks, v) = Object $ insertWith (\_ o' → assign o' (k' : ks, v)) k (assign (object []) (k' : ks, v)) o
-        assign x _ = x
-        concatJSON (Array v1) (Array v2) = Array $ v1 ++ v2
-        concatJSON (Array v1) _ = Array v1
-        concatJSON _ (Array v2) = Array v2
-        concatJSON _ _ = Null
-
-instance FromFormUrlEncoded MicropubRequest where
-  fromFormUrlEncoded f =
-    case lookup "mp-action" f of
-      Nothing → let (Object o') = formToObject f
-                    o = deleteMap "h" o'
-                    h = "h-" ++ fromMaybe "entry" (lookup "h" f) in
-                      Right $ Create h o []
-      _ → Left "Unknown action type"

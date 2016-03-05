6052b6c3dab963fbe3a20fdc90613dc82558cc67
Greg V <greg@unrelenting.technology>
2016-03-05 17:47:44 +0300

    improve and fix slice

--- library/Sweetroll/Slice.hs
+++ library/Sweetroll/Slice.hs
@@ -12,13 +12,17 @@ import           Sweetroll.Util
 sliceCategory ∷ Int → Maybe Int → Maybe Int → CategoryName → [(Int, String)] → Slice URI
 sliceCategory perPage before after catName unsortedKeys =
   let allKeys = sortOn (negate . fst) unsortedKeys
-      mayFilterFst f (Just y) xs = filter (f y . fst) xs
-      mayFilterFst _ Nothing  xs = xs
-      items = take perPage $ mayFilterFst (<) after $ mayFilterFst (>) before allKeys in
+      rangeFilter = case (before, after) of
+                      (Just b, Just a)   → take perPage . filter (\(k, _) → k < b && k > a)
+                      (Just b, Nothing)  → take perPage . filter (\(k, _) → k < b)
+                      (Nothing, Just a)  → reverse . take perPage . reverse . filter (\(k, _) → k > a)
+                      (Nothing, Nothing) → take perPage
+      items = rangeFilter allKeys in
       Slice {
         sliceItems  = map (permalink (Proxy ∷ Proxy EntryRoute) catName . snd) items
       , sliceBefore = case (fst <$> lastMay allKeys, fst <$> lastMay items) of
-                        (Just minId, Just lastId) | lastId > minId → Just $ permalink (Proxy ∷ Proxy CatRouteB) catName lastId
+                        (Just minId, Just lastId) | lastId > minId →
+                          Just $ permalink (Proxy ∷ Proxy CatRouteB) catName lastId
                         _ → Nothing
       , sliceSelf   = case (before, after) of
                         (Just b, Just a)   → permalink (Proxy ∷ Proxy CatRoute)  catName b a
@@ -26,5 +30,6 @@ sliceCategory perPage before after catName unsortedKeys =
                         (Nothing, Just a)  → permalink (Proxy ∷ Proxy CatRouteA) catName a
                         (Nothing, Nothing) → permalink (Proxy ∷ Proxy CatRouteE) catName
       , sliceAfter  = case (fst <$> headMay allKeys, fst <$> headMay items) of
-                        (Just maxId, Just headId) | headId < maxId → Just $ permalink (Proxy ∷ Proxy CatRouteA) catName headId
+                        (Just maxId, Just headId) | headId < maxId →
+                          Just $ permalink (Proxy ∷ Proxy CatRouteA) catName headId
                         _ → Nothing }
